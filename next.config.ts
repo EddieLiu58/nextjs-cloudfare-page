cbeb3d86f4a0ec99e93e563cc54e45c61fed477f
EddieLiu <eddieliu58@gmail.com>
2024-11-02 15:02:45 +0800

    feat: init next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next';
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-if (process.env.NODE_ENV === 'development') {
-	await setupDevPlatform();
-}
+const nextConfig: NextConfig = {
+	/* config options here */
+};
 
 export default nextConfig;
